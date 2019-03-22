a746d22a29242eb5d7bc09f2b735ee88ec937344
Thomas Güttler <guettliml@thomas-guettler.de>
2019-03-22 11:17:07 +0100

    Yes: now visible: random is much slower.

--- bench-bytea-inserts-postrgres.py
+++ bench-bytea-inserts-postrgres.py
@@ -1,5 +1,7 @@
 import os
 import time
+import matplotlib.pyplot as plt
+import numpy
 
 import psycopg2
 
@@ -13,15 +15,25 @@ create table bytea_test (
 ;
 ''')
 random_stream = open('/dev/urandom', 'rb')
-for i in range(0, 1*10**9, 100000):
-    start_time = time.time()
+rows=[]
+for i in range(0, 1*10**8, 10000000):
     data = psycopg2.Binary(random_stream.read(i))
+    start_time = time.time()
     cursor.execute('''insert into bytea_test values (%s)''', (data,))
     duration_random = time.time()-start_time
 
-    start_time = time.time()
     data = 'x'*i
+    start_time = time.time()
     cursor.execute('''insert into bytea_test values (%s)''', (data,))
     duration_ascii = time.time()-start_time
-
-    print('{};{};{}'.format(i, duration_ascii, duration_random))
+    kilo_bytes = i / 1000
+    row = (kilo_bytes, duration_ascii, duration_random)
+    print('{},'.format(row))
+    rows.append(row)
+m = numpy.array(rows).transpose()
+plt.plot(m[0], m[1], label='ascii')
+plt.plot(m[0], m[2], label='random')
+plt.legend()
+plt.xlabel('kilo bytes insert into bytea column')
+plt.ylabel('duriation (seconds)')
+plt.show()
